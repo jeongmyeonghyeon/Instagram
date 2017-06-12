7f419733057773c6f6c76b16b0ab8a34ef9390a4
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-06-12 14:20:00 +0900

    PostLike, Comment model 추가

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -16,9 +16,37 @@ class Post(models.Model):
     modified_date = models.DateTimeField(auto_now_add=True)
     like_users = models.ManyToManyField(
         User,
-        related_name='like_posts'
+        related_name='like_posts',
+        through='PostLike'
     )
     tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        # 자신을 post로 갖고, 전달받은 user를 author로 가지며
+        # content를 content필드내용으로 넣는 Comment객체 생성
+        return self.comment_set.create(author=user, comment=content)
+
+    def add_tag(self, tag_name):
+        # tags에 tag매개변수로 전달된 값(str)을
+        # name으로 갖는 Tag객체를 (이미지 존재하면) 가져오고 없으면 생성하여
+        # 자신의 tags에 추가
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+        if not self.tags.filter(name=tag_name).exists():
+            self.tags.add(tag)
+
+    @property
+    def like_count(self):
+        # 자신을 like하고 있는 user수 리턴
+        return self.like_users.count()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey('Post')
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'post_post_like'
 
 
 class Comment(models.Model):
@@ -30,16 +58,17 @@ class Comment(models.Model):
     comment = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        through='CommentLike',
+        related_name='Like_comments'
+    )
 
 
-# class PostLike(models.Model):
-#     like_or_not = models.BooleanField(default=False)
-#     posts = models.ForeignKey(
-#         'Post'
-#     )
-#
-#     def __str__(self):
-#         return self.like_or_not
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Tag(models.Model):
